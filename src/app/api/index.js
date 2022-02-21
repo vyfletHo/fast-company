8819a548c03a48f45f9bdbef631649af99e7f7eb
Mikhail Filimonov <mixail.filimonov@bk.ru>
2022-02-21 20:55:11 +0400

    html

--- src/app/api/index.js
+++ src/app/api/index.js
@@ -1,9 +1,11 @@
 import users from "./fake.api/user.api";
 import professions from "./fake.api/professions.api";
 import qualities from "./fake.api/qualities.api";
+import comments from "./fake.api/commets.api";
 const API = {
     users,
     professions,
-    qualities
+    qualities,
+    comments
 };
 export default API;
